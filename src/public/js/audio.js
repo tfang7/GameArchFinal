aff49f10e5dffad2ebd3f223548b2b8b9021b7eb
tfang7 <dkfang7@gmail.com>
2016-04-29 11:50:53 -0400

    some updates

--- src/public/js/audio.js
+++ src/public/js/audio.js
@@ -1,9 +1,12 @@
 var context;
 var source, sourceJs;
-var analyser;
+var analyser,analyser2;
+var splitter;
 var url = 'public/data/HappyDays.mp3';
 var array = new Array();
 var boost = 0;
+
+var ctx = $("#spectrogram").get()[0].getContext("2d");
 
 var interval = window.setInterval(function() {
 	if($('#loading_dots').text().length < 3) {
@@ -15,12 +18,7 @@ var interval = window.setInterval(function() {
 }, 500);
 
 try {
-	if(typeof webkitAudioContext === 'function' || 'webkitAudioContext' in window) {
-		context = new webkitAudioContext();
-	}
-	else {
-		context = new AudioContext();
-	}
+	context = new AudioContext();
 }
 catch(e) {
 	$('#info').text('Web Audio API is not supported in this browser');
@@ -41,14 +39,28 @@ request.onload = function(){
 	sourceJs = context.createScriptProcessor(2048, 1, 1);
 	sourceJs.buffer = buffer;
 	sourceJs.connect(context.destination);
-	analyser = context.createAnalyser();
-	analyser.smoothingTimeConstant = 0.6;
-	analyser.fftSize = 512;
-	
+	//first analyser
+    analyser = context.createAnalyser();
+	analyser.smoothingTimeConstant = 0.3;
+	analyser.fftSize = 1024;
+	//second
+    analyser2 = context.createAnalyser();
+    analyser2.smoothingTimeConstant = 0.0;
+    analyser2.fftSize = 1024;
+    
+    
+    //Spectrogram
+    
+    
 	source = context.createBufferSource();
+    splitter = context.createChannelSplitter();
+    source.connect(splitter);
 	source.buffer = buffer;
 	source.loop = true;
-	
+	//splitter
+    splitter.connect(analyser,0,0);
+    splitter.connect(analyser2,1,0);
+    
 	source.connect(analyser);
 	analyser.connect(sourceJs);
 	source.connect(context.destination);
@@ -56,6 +68,9 @@ request.onload = function(){
 	sourceJs.onaudioprocess = function(e){
 		array = new Uint8Array(analyser.frequencyBinCount);
 		analyser.getByteFrequencyData(array);
+       // var average = getAverageVolume(array);
+        
+        
 	};
     clearInterval(interval);
 
@@ -84,119 +99,7 @@ request.send();
 function play() {
 	$('#play').fadeOut('normal', function() {
 		$(this).remove();
+        $('info').remove();
 	});
 	source.start(0);
 }
-/*
-var context;
-var source, sourceJs;
-var analyser;
-var url = 'public/data/HappyDays.mp3';
-var array = new Array();
-var boost = 0;
-
-var interval = window.setInterval(function() {
-	if($('#loading_dots').text().length < 3) {
-		$('#loading_dots').text($('#loading_dots').text() + '.');
-	}
-	else {
-		$('#loading_dots').text('');
-	}
-}, 500);
-
-try {
-	if(typeof webkitAudioContext === 'function' || 'webkitAudioContext' in window) {
-		context = new webkitAudioContext();
-	}
-	else {
-		context = new AudioContext();
-	}
-}
-catch(e) {
-	$('#info').text('Web Audio API is not supported in this browser');
-}
-var request = new XMLHttpRequest();
-request.open("GET", url, true);
-request.responseType = "arraybuffer";
-
-request.onload = function() {
-	context.decodeAudioData(
-		request.response,
-		function(buffer) {
-			if(!buffer) {
-				$('#info').text('Error decoding file data');
-				return;
-			}
-
-			sourceJs = context.createScriptProcessor(2048, 1, 1);
-			sourceJs.buffer = buffer;
-			sourceJs.connect(context.destination);
-			analyser = context.createAnalyser();
-			analyser.smoothingTimeConstant = 0.6;
-			analyser.fftSize = 512;
-
-			source = context.createBufferSource();
-			source.buffer = buffer;
-			source.loop = true;
-
-			source.connect(analyser);
-			analyser.connect(sourceJs);
-			source.connect(context.destination);
-
-			sourceJs.onaudioprocess = function(e) {
-				array = new Uint8Array(analyser.frequencyBinCount);
-				analyser.getByteFrequencyData(array);
-
-			};
-
-			
-			clearInterval(interval);
-
-			// popup
-			$('body').append($('<div onclick="play();" id="play" style="width: ' + $(window).width() + 'px; height: ' + $(window).height() + 'px;"><div id="play_link"></div></div>'));
-			$('#play_link').css('top', ($(window).height() / 2 - $('#play_link').height() / 2) + 'px');
-			$('#play_link').css('left', ($(window).width() / 2 - $('#play_link').width() / 2) + 'px');
-			$('#play').fadeIn();
-		},
-		function(error) {
-			$('#info').text('Decoding error:' + error);
-		}
-	);
-};
-
-request.onerror = function() {
-	$('#info').text('buffer: XHR error');
-};
-
-request.send();
-
-function displayTime(time) {
-	if(time < 60) {
-		return '0:' + (time < 10 ? '0' + time : time);
-	}
-	else {
-		var minutes = Math.floor(time / 60);
-		time -= minutes * 60;
-		return minutes + ':' + (time < 10 ? '0' + time : time);
-	}
-}
-
-function play() {
-	$('#play').fadeOut('normal', function() {
-		$(this).remove();
-	});
-	source.start(0);
-}
-
-$(window).resize(function() {
-	if($('#play').length === 1) {
-		$('#play').width($(window).width());
-		$('#play').height($(window).height());
-
-		if($('#play_link').length === 1) {
-			$('#play_link').css('top', ($(window).height() / 2 - $('#play_link').height() / 2) + 'px');
-			$('#play_link').css('left', ($(window).width() / 2 - $('#play_link').width() / 2) + 'px');
-		}
-	}
-});
-*/
