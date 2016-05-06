ed0b87c955347510f09512881a180acb73ce3f7c
tfang7 <dkfang7@gmail.com>
2016-05-06 00:09:52 -0400

    update faces function

--- src/public/js/TerrainMaker.js
+++ src/public/js/TerrainMaker.js
@@ -1,3 +1,4 @@
+//console.log("Test");
 
 //SCENE SETUP
 var width  = window.innerWidth/2,
@@ -11,7 +12,7 @@ var renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(width, height);
 
 //CREATE THE LANDSCAPE
-var terrain = new THREE.PlaneGeometry(60, 60, 25, 25);
+var terrain = new THREE.PlaneGeometry(60, 60, 20, 20);
 for (var i = 0; i < terrain.vertices.length; i++) {
     terrain.vertices[i].z = 0;
 }
@@ -21,45 +22,103 @@ var material = new THREE.MeshPhongMaterial({
 });
 
 var plane = new THREE.Mesh(terrain, material);
-plane.geometry.dynamic = true;
-plane.geometry.verticesNeedUpdate = true;
-plane.geometry.normalsNeedUpdate = true;
 scene.add(plane);
-
+var edges = new THREE.FaceNormalsHelper( plane, 2, 0x00ff00, 1 );
+scene.add(edges);
 //ADD CAMERA CONTROLS
 var controls = new THREE.TrackballControls(camera); 
 document.getElementById('webgl').appendChild(renderer.domElement);
 render();
 
-
 function render() {
     //RENDER EVERYTHING
-    console.log("Array: " + array.length);
+   // console.log("Array: " + array.length);
     controls.update();    
     requestAnimationFrame(render);
     renderer.render(scene, camera);
-    if((array.length > 0) && playing){
-        updateTerrain();
-    }
-}
-
-function updateTerrain(){
-    scene.remove(plane);
+    if(array.length > 0){
         
-    terrain = new THREE.PlaneGeometry(60, 60, 25, 25);
-    for (var i = 0; i < terrain.vertices.length; i++) {
-        var height = array[i];
-        if(height != 0)
-            height = height/25;
-        terrain.vertices[i].z = height;
+        scene.remove(plane);
+        
+        terrain = new THREE.PlaneGeometry(60, 60, 20, 20);
+        for (var i = 0; i < terrain.vertices.length; i++) {
+            var height = array[i];
+            if(height != 0)
+                height = height/25;
+            terrain.vertices[i].z = height;
+            
+        }
+        UpdateFaces(terrain);
+        var material = new THREE.MeshBasicMaterial({
+            vertexColors:THREE.VertexColors
+        });
+        plane = new THREE.Mesh(terrain, material);
+        edges = new THREE.FaceNormalsHelper( plane, 2, 0x00ff00, 1 );
+
+        //plane.geometry.dynamic = true;
+        //plane.geometry.verticesNeedUpdate = true;
+        //plane.geometry.normalsNeedUpdate = true;
+        plane.geometry.dynamic = true;
+        plane.geometry.verticesNeedUpdate = true;
+        plane.geometry.normalsNeedUpdate = true;
+        plane.geometry.colorsNeedUpdate = true;
+        plane.dynamic = true;
+        
+
+        scene.add(plane);
+        
+        
+        
+        /*console.log(plane.geometry.vertices.length);
+        for (var i = 0; i < plane.geometry.vertices.length; i++) {
+            //console.log("Index " + i + ": " + array[i]);
+            plane.geometry.vertices[i].z = array[i];
+        }
+        scene.remove(plane);
+        scene.add(plane);*/
     }
-    var material = new THREE.MeshPhongMaterial({
-        color: 0x49C960, 
-        wireframe: true
-    });
-
-    plane = new THREE.Mesh(terrain, material);
-
-    scene.add(plane);
 }
+
+
+function UpdateFaces(terrainMap){
+ var vertexIndex;
+
+    var faceIndices = [ 'a', 'b', 'c' ];
+    var color, f, p, vertexIndex,
+					radius = 200;
+    
+    for (var i = 0; i < terrainMap.faces.length; i ++ ) {
+        f = terrainMap.faces[i];
+        for (var j = 0; j < 3; j++){
+          //  console.log(
+         vertexIndex = f[ faceIndices[j] ];
+         p = terrainMap.vertices[ vertexIndex ];
+            
+         color = new THREE.Color(0xff0000);
+         color.setHSL( ( p.y / radius + 1 ) / 2, 1.0, 0.5 );
+         f.vertexColors[ j ] = color;
+        }
+        
+    }
+    terrainMap.computeFaceNormals();
+
+
+}     
+    /*for (var i = 0; i < terrainMap.faces.length; i ++ ) {
+
+        f  = terrainMap.faces[ i ];
+        console.log(f);
+        /*for( var j = 0; j < 3; j++ ) {
+
+            vertexIndex = f[ faceIndices[ j ] ];
+
+            p = terrainMap.vertices[ vertexIndex ];
+
+            color = new THREE.Color( 0xffffff );
+            color.setHSL( ( p.y / radius + 1 ) / 2, 1.0, 0.5 );
+            f.vertexColors[ j ] = color;
+        }
+
+    }    */
+    
 
